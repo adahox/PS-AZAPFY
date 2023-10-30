ad6b85433f5ba493e5258e7abfcba2ac57d65c2a
Adão Dias <adahox@gmail.com>
2023-10-30 15:38:24 -0300

    setup babel + css loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,39 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-
+    mode: 'development',
     entry: './src/index.js',
+    devServer: {
+        static: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.s?css$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ],
+            },
+        ]
+    },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
